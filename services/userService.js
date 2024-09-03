568b44766a83ae9928505de0974f785f5b71c613
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-09-03 11:23:35 +0530

    add messages constants error types response utils functions

--- services/userService.js
+++ services/userService.js
@@ -3,23 +3,23 @@ import { UserModel } from "../models/userModel.js";
 const userService = {};
 
 userService.findUserByIdInDB = async(userId) => {
-    return await userModel.findOne({ userId });
+    return await UserModel.findOne({ userId });
 };
 userService.findUserInDB = async(email, mobileNumber) => {
-    return await userModel.findOne({ $or: [{ email: email }, { mobileNumber: mobileNumber }] });
+    return await UserModel.findOne({ $or: [{ email: email }, { mobileNumber: mobileNumber }] });
 };
 
 userService.saveUser = async(userDetailsObject) => {
-    const userDetails = new userModel(userDetailsObject);
+    const userDetails = new UserModel(userDetailsObject);
     return await userDetails.save();
 };
 
 userService.verifyUser = async(userId) => {
-    await userModel.findOneAndUpdate({ _id: userId }, { isOtpVerified: true }, { new: true });
+    await UserModel.findOneAndUpdate({ _id: userId }, { isOtpVerified: true }, { new: true });
 };
 
 userService.resetPasswordInDb = async(userId, hashedPassword) => {
-    const user = await userModel.findById(userId);
+    const user = await UserModel.findById(userId);
     if (!user) {
         return { success: false, message: "User does not exist" };
     }
